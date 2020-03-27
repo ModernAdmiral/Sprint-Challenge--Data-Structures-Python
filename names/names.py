12146781600d12221c03ea1543fa12cdd961818e
Jackson McComas <jackson.mccomas@gmail.com>
2020-03-27 13:17:21 -0500

    touches

--- names/names.py
+++ names/names.py
@@ -12,7 +12,7 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-search_tree = BinarySearchTree(names_1[0])
+search_tree = BinarySearchTree(names_1[0])  # put in the first name to start
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:  # loop though each name in in names_1.txt
     # create a binary search tree out of those names
@@ -26,6 +26,10 @@ for name_2 in names_2:  # seperate loop though names_2.txt
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
+
+# OLD CODE  == O(n^2)
+# NEW CODE  == O(n log n)
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
