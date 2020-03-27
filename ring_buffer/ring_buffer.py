af66104afbc346617542b39bf164a995096aa2e9
Jackson McComas <jackson.mccomas@gmail.com>
2020-03-27 15:03:55 -0500

    ring buffer in progress. not MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,8 +7,17 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
-    def append(self, item):
-        pass
+    def append(self, item):  # need to append before we get
+        # if ring buffer is empty, and current isnt set, set current to be head
+        if len(self.storage) < self.capacity and self.current is None:
+            self.current = self.storage.head
+
+        # if ring buffer isnt at capacity, just append new item to storage
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        # if we are at capacity,
+        if len(self.storage) == self.capacity:
+            if self.current.next is None:
 
     def get(self):
         # Note:  This is the only [] allowed
